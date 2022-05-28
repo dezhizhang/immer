8be4cb999a657591218e14274a64b2c7af9e2e6e
dezhizhang <1541609448@qq.com>
2022-05-29 05:16:56 +0800

    feat:update

--- src/immer/index.ts
+++ src/immer/index.ts
@@ -1,43 +1,61 @@
-import { isObject,isArray } from './is';
+import { isObject, isArray } from './is';
 const INTERNAL = Symbol('INTERNAL');
 
-export function produce(baseState:any,producer:any) {
+export function produce(baseState: any, producer: any) {
     let proxy = toProxy(baseState);
+    producer(proxy);
+    const internal = proxy[INTERNAL];
+    return internal.mutated ? internal.draftState:internal.baseState
 }
 
-export function toProxy(baseState:any) {
-    let keyToProxy:any = {};
+export function toProxy(baseState: any,callParentCopy?:Function) {
+    let keyToProxy: any = {};
     let internal = {
         baseState,
-        draftState:createDraftState(baseState),
+        draftState: createDraftState(baseState),
         keyToProxy,
-        mutated:false,
-    }
-    return new Proxy(baseState,{
-        get(target,key) {
-            if(key === INTERNAL) {
+        mutated: false,
+    };
+    return new Proxy(baseState, {
+        get(target, key) {
+            if (key === INTERNAL) {
                 return internal;
             }
             let value = target[key];
-            if(isObject(value) || isArray(value)) {
-                if(key in keyToProxy) {
+            if (isObject(value) || isArray(value)) {
+                if (key in keyToProxy) {
                     return keyToProxy[key];
-                }else {
-                    keyToProxy[key] = toProxy(value);
+                } else {
+                    keyToProxy[key] = toProxy(value,() => {
+                        internal.mutated = true;
+                        const proxyChild = keyToProxy[key];
+                        let { draftState:childDraftState } = proxyChild[INTERNAL];
+                        internal.draftState = childDraftState;
+                        callParentCopy && callParentCopy();
+                    });
                 }
                 return keyToProxy[key];
             }
-            return internal.mutated ? (internal.draftState as any)[key]:internal.baseState[key]
+            return internal.mutated
+                ? (internal.draftState as any)[key]
+                : internal.baseState[key];
         },
-        set(target,key,value) {
+        set(target, key, value) {
             internal.mutated = true;
             const { draftState } = internal;
-            (draftState as any)[key] = value
-            return false;
-        }
-    })
+            (draftState as any)[key] = value;
+            callParentCopy && callParentCopy();
+            return true;
+        },
+    });
 }
 
-function createDraftState(baseState:any) {
-
+function createDraftState(baseState: any) {
+    if (isObject(baseState)) {
+        return Object.assign({}, baseState);
+    } else if (isArray(baseState)) {
+        return [...baseState];
+    } else {
+        return baseState;
+    }
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { produce} from 'immer';
+import { produce} from '../immer/index';
 
 let baseState = {
   list:['1']
